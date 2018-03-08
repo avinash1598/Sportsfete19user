44611cbef8caf1727ed35ca99c10548e2789965c
avinash1598 <madrupam@gmail.com>
2018-03-08 22:11:40 +0530

    json file changed

--- app/src/main/java/spider/app/sportsfete18/SportDetails/RuleBookFragment.java
+++ app/src/main/java/spider/app/sportsfete18/SportDetails/RuleBookFragment.java
@@ -22,6 +22,7 @@ public class RuleBookFragment extends Fragment {
 
     private static final String TAG="RuleBookFragment";
     String selectedSport;
+    TextView alertbox_content;
 
 
     public RuleBookFragment() {
@@ -43,170 +44,170 @@ public class RuleBookFragment extends Fragment {
         FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(getActivity().getApplicationContext());
         Bundle bundle = new Bundle();
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "RuleBook");
-        mFirebaseAnalytics.logEvent("RuleBook Fragment",bundle);
+        mFirebaseAnalytics.logEvent("RuleBook_Fragment",bundle);
         TextView alertbox_title = (TextView)
                 view.findViewById(R.id.title);
         if(selectedSport.equals("0"))
         {
             alertbox_title.setText("Athletics");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Athletics), null, new MyTagHandler()));
         }
         else if(selectedSport.equals("1"))
         {
             alertbox_title.setText("Badminton");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Badminton), null, new MyTagHandler()));
         }
         if(selectedSport.equals("2"))
         {
             alertbox_title.setText("Basketball");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Basketball), null, new MyTagHandler()));
         }
         if(selectedSport.equals("3"))
         {
             alertbox_title.setText("Basketball");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Basketball_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("4"))
         {
             alertbox_title.setText("Carrom");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Carrom), null, new MyTagHandler()));
         }
         if(selectedSport.equals("5"))
         {
             alertbox_title.setText("Chess");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Chess), null, new MyTagHandler()));
         }
         if(selectedSport.equals("6"))
         {
             alertbox_title.setText("Cricket");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Cricket), null, new MyTagHandler()));
         }
         if(selectedSport.equals("7"))
         {
             alertbox_title.setText("Cricket");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Cricket_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("8"))
         {
             alertbox_title.setText("Football");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Football), null, new MyTagHandler()));
         }
         if(selectedSport.equals("9"))
         {
             alertbox_title.setText("Football");
-            TextView alertbox_content = (TextView)view.findViewById(R.id.content);
+            alertbox_content = (TextView)view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Football_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("10"))
         {
             alertbox_title.setText("Handball");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Handball_Boys), null, new MyTagHandler()));
         }
         if(selectedSport.equals("11"))
         {
             alertbox_title.setText("Handball");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Handball_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("12"))
         {
             alertbox_title.setText("Hockey");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Hockey), null, new MyTagHandler()));
         }
         if(selectedSport.equals("13"))
         {
             alertbox_title.setText("Kabaddi");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Kabaddi), null, new MyTagHandler()));
         }
         if(selectedSport.equals("14"))
         {
             alertbox_title.setText("Kho Kho");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.KhoKho), null, new MyTagHandler()));
         }
         if(selectedSport.equals("15"))
         {
             alertbox_title.setText("Kho Kho");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.KhoKho_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("16"))
         {
             alertbox_title.setText("Marathon");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Marathon), null, new MyTagHandler()));
         }
         if(selectedSport.equals("17"))
         {
             alertbox_title.setText("Power Lifting");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.PowerLifting), null, new MyTagHandler()));
         }
         if(selectedSport.equals("18"))
         {
             alertbox_title.setText("Swimming");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Swimming), null, new MyTagHandler()));
         }
         if(selectedSport.equals("19"))
         {
             alertbox_title.setText("Table Tennis");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.TableTennis), null, new MyTagHandler()));
         }
         if(selectedSport.equals("20"))
         {
             alertbox_title.setText("Table Tennis");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.TableTennis_girls), null, new MyTagHandler()));
         }
 
         if(selectedSport.equals("21"))
         {
             alertbox_title.setText("Tennis");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Tennis), null, new MyTagHandler()));
         }
         if(selectedSport.equals("22"))
         {
             alertbox_title.setText("Tennis");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Tennis_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("23"))
         {
             alertbox_title.setText("Throwball");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Throwball), null, new MyTagHandler()));
         }
         if(selectedSport.equals("24"))
         {
             alertbox_title.setText("Volley Ball");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Volleyball), null, new MyTagHandler()));
         }
         if(selectedSport.equals("25"))
         {
             alertbox_title.setText("Volley Ball");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.Volleyball_Girls), null, new MyTagHandler()));
         }
         if(selectedSport.equals("26"))
         {
             alertbox_title.setText("Water Polo");
-            TextView alertbox_content = (TextView) view.findViewById(R.id.content);
+            alertbox_content = (TextView) view.findViewById(R.id.content);
             alertbox_content.setText(Html.fromHtml(getString(R.string.WaterPolo), null, new MyTagHandler()));
         }
     }
@@ -220,6 +221,13 @@ public class RuleBookFragment extends Fragment {
             result = Html.fromHtml(html);
         }
         return result;
+    }
+
+    @Override
+    public void onDestroyView(){
+        alertbox_content.setText("");
+        Runtime.getRuntime().gc();
+        super.onDestroyView();
     }
 
     }
